66d12ad76395ffb6975e0e36bd5bd7c193c83368
Mathias Maaß <mathiasmaass@gmx.de>
2016-04-17 19:50:51 +0200

    menu now can draw a clickable marker list

---
+++ menu.js
@@ -0,0 +1,55 @@
+
+var menu = {
+
+    /**
+    * Creates a clickable list of the stored markers.
+    * A click on a item invokes the marking process.
+    */
+    draw: function() {
+
+        var that = this;
+
+        chrome.runtime.getBackgroundPage(function (bg) {
+
+            bg.markerdb.get(null, function(markers) {
+
+                for (key in markers) {
+                    
+                    var item = that._addItemToList(markers[key].title);
+                    that._createItemCallback(item, markers[key].id);
+                }
+            });
+        });
+    },
+
+    /** 
+    * Puts a new item to the marker list.
+    * 
+    * @param {String} title - title of the marker to add
+    * @return {DOM Element} - reference to the new item
+    */
+    _addItemToList: function(title) {
+    
+        var item = document.createElement('p');
+        var text = document.createTextNode(title);
+        item.appendChild(text);
+        var list = document.getElementById('marker-list');
+        list.appendChild(item);
+        return item;
+    },
+
+    _createItemCallback: function(item, id) {
+
+        item.addEventListener('click', function() {
+        
+            chrome.runtime.getBackgroundPage(function(bg) {
+
+                bg.tests.logSelectedMarker(id);                
+            });  
+        });        
+    }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    menu.draw();
+});
---
+++ tests.js
@@ -0,0 +1,7 @@
+
+var tests = {
+
+    logSelectedMarker: function(id) {
+        console.log(id);
+    }
+};
