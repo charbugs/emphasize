0b6fa298b1476ef9d7d191ec884c1f114c2bf887
Mathias <mathiasmaass@gmx.de>
2016-07-19 23:07:27 +0200

    module control: finished interface applyMarker.

--- content-control.js
+++ content-control.js
@@ -1,5 +1,12 @@
 
 chrome.runtime.onMessage.addListener(function(message, sender, callback) {
-	console.log('From: ' + sender.url);
-	console.log('Message: ' + message);
+	
+	if (message.command === 'alive')
+		callback(1);
+	else if (message.command === 'apply')
+		extract.extract();
+	else
+		console.log('content-control: unknown command: ' + message.command);
+	
+	debugger;
 });
--- extension-control.js
+++ extension-control.js
@@ -7,6 +7,8 @@ var extensionControl = {
 	activeTab: null,
 	/* ID of chosen marker */
 	markerId: null,
+	/* content scripts to inject in given order*/
+	scripts: ['tokenize.js', 'extract.js', 'content-control.js'],
 
 	applyMarker: function(id) {
 
@@ -15,36 +17,47 @@ var extensionControl = {
 
 		chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-			that.activeTab = tabs[0].id
+			that.activeTab = tabs[0].id;
+			var message = {command: 'alive'};
 
-			if (that.injectedTabs.indexOf(that.activeTab) === -1) {
+			chrome.tabs.sendMessage(that.activeTab, message, function (respond) {
 
-				that.injectedTabs.push(that.activeTab);
-				
-				chrome.tabs.executeScript(
-					that.activeTab, 
-					{ file:'content-control.js' }, 
-					that.passToContentControl.bind(that)
-				);	
-			}
-			else {
-				
-				that.passToContentControl();
-			}
-				
+				if (respond)
+					that._passMarkerToContentControl();
+				else
+					that._executeScripts();
+			});
 		});
 	},
 
-	passToContentControl: function() {
+	_passMarkerToContentControl: function() {
 
 		var that = this;
 
 		markerdb.get(that.markerId, function (marker) {
 			
 			var message = {command: 'apply', data: marker};
-
 			chrome.tabs.sendMessage(that.activeTab, message);
 		});
 		
+	},
+
+	_executeScripts: function() {
+
+		var that = this;
+
+    	function createCallback(tabId, script, callback) {
+        	return function () {
+            	chrome.tabs.executeScript(tabId, {file: script}, callback);
+        	};
+        }
+
+    	var callback = that._passMarkerToContentControl.bind(that);
+
+    	for (var i = that.scripts.length - 1; i >= 0; --i)
+        	callback = createCallback(that.activeTab, that.scripts[i], callback);
+
+    	if (callback !== null)
+        	callback();   // execute outermost function
 	}	
 };
