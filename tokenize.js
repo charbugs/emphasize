4539bd141cf91dde035c623b16846e5140ca5540
Mathias <mathiasmaass@gmx.de>
2016-07-23 22:25:28 +0200

    module 'tokenizer' now fits the js module pattern

--- tokenize.js
+++ tokenize.js
@@ -1,24 +1,24 @@
 
-var tokenize = {
+var tokenize = (function() {
 
-    PERIOD: '.',
+    const PERIOD = '.';
 
-    PUNCTS: [	
+    const PUNCTS = [	
         '?', '!', '"', "'", ',', ';', ':', '(',	')', '{', '}', 
         '%', '/', '[', ']',
         String.fromCharCode(8216), String.fromCharCode(8217),
         String.fromCharCode(8218), String.fromCharCode(8220),
         String.fromCharCode(8221), String.fromCharCode(8222),
         String.fromCharCode(8230)
-    ],
+    ];
 
-    ABBREVS: [
+    const ABBREVS = [
         'allg.', 'bzw.', 'bspw.', 'b.w.', 'd. h.', 'd. i.', 'etc.',
         'evtl.', 'geb.', 'ggf.', 'n. Chr.', 'od.', 's.', 's. a.',
         's.o.', 's.u.', 'u.', 'u. a.', 'u. Ä.', 'u.U.', 'u.z.', 'u.a.',
         'usw.', 'v. a.', 'v. Chr.', 'vgl.', 'z.B.', 'z.T.', 'zB.', 'zb.',
         'z.Zt.', 'i.d.R.', 'i.A.', 'z.Hd.' 
-    ],
+    ];
 
     /**
     * Respesents a single Token.
@@ -26,11 +26,11 @@ var tokenize = {
     * @param {String} form - the literal form of the token
     * @param {Boolean} isAffix - is this token an suffix of the previous token?
     */
-    Token: function(form, isSuffix) {
+    function Token(form, isSuffix) {
 
         this.form = form;
         this.isSuffix = isSuffix;
-    },
+    }
 
     /**
     * Split a string in single tokens.
@@ -38,46 +38,46 @@ var tokenize = {
     * @param {String} string - string to tokenize
     * @return {Array of Token}
     */
-    split: function(string) {
+    function split(string) {
 
-        var tokens = string.trim().split(/\s+/).map(this._splitPuncts, this);
+        var tokens = string.trim().split(/\s+/).map(splitPunctsFromToken);
         return [].concat.apply([], tokens);
-    },
+    }
 
     /**
-    * Splits an expression if it contains leading an trailing punctuation.
+    * Splits an token when it contains leading an trailing punctuation.
     *
     * @param {String} token - a string without withspaces
     * @return {Array of Token}
     */
-    _splitPuncts: function(token) {
+    function splitPunctsFromToken(token) {
 
         // A single charakter is a proper token.
         if (token.length == 1)
-            return [new this.Token(token, false)];
+            return [new Token(token, false)];
 
         // First split the token in head chars, tail chars,
         // the actual word and the period.
         var head = [], tail = [], period = [], word = [];
 
-        while (this.PUNCTS.indexOf(token[0]) > -1) {
+        while (PUNCTS.indexOf(token[0]) > -1) {
 
             head.push(token[0]);
             token = token.slice(1);
         }
 
-        while (this.PUNCTS.indexOf(token[token.length-1]) > -1) {
+        while (PUNCTS.indexOf(token[token.length-1]) > -1) {
 
             tail.push(token[token.length-1]);
             token = token.slice(0,-1);
         }
 
         if (token.length > 1 && 
-	            token[token.length-1] == this.PERIOD && 
-	        	this.ABBREVS.indexOf(token) == -1) {
+	            token[token.length-1] == PERIOD && 
+	        	ABBREVS.indexOf(token) == -1) {
 
             word = token.slice(0,-1);
-            period = [this.PERIOD];
+            period = [PERIOD];
         }
         else
             if(token)
@@ -88,12 +88,12 @@ var tokenize = {
         return splitted.map(function(item, index) {
 
             if (index === 0) 
-                return new this.Token(item, false);
+                return new Token(item, false);
             else 
-                return new this.Token(item, true);
+                return new Token(item, true);
 
-        }, this);
-    },
+        });
+    }
 
     /**
     * Concatenates tokens to the original string.
@@ -101,7 +101,7 @@ var tokenize = {
     * @param {Array of Token} tokens
     * @return {String}
     */
-    concat: function(tokens) {
+    function concat(tokens) {
 
         var string = '';
 
@@ -113,6 +113,12 @@ var tokenize = {
 
         return string;
     }
-};
+
+    return {
+        split: split,
+        concat: concat
+    };
+
+}());
 
 
