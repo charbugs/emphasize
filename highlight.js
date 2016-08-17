16ae4c1f7474665827133e89891434bfcced53fd
Mathias <mathiasmaass@gmx.de>
2016-08-18 01:30:46 +0200

    Refactored module highlight

--- highlight.js
+++ highlight.js
@@ -1,240 +1,290 @@
-
+/** 
+* @module highlight 
+*/
 var highlight = (function() {
 
-	const GLOBAL_CLASS_NAME = 'vink-element';
+    // Class name that identifies elements added by vink
+    const GLOBAL_CLASS_NAME = 'vink-element';
 
-	const pencilMap = {
+    // Class names for different highlighting styles
+    const pencilMap = {
+        1: 'vink-pen-1',
+        2: 'vink-pen-1',
+        3: 'vink-pen-2',
+        4: 'vink-pen-2'
+    };
 
-		1: 'vink-pen-1',
-		2: 'vink-pen-1',
-		3: 'vink-pen-2',
-		4: 'vink-pen-2'
-	};
+    /**
+    * Highlights tokens of the web page according to a nummerical mask
+    * that declares which tokens to be highlighted.
+    * @see documentation for further explanations
+    *
+    * @param {Array of tokenize.Tokens} tokens
+    * @param {Array of Number} mask
+    */
+    function highlight(tokens, mask) {
 
-	function highlight(tokens, mask) {
+        // Mapping from text nodes of the web page that are affected by 
+        // higlighting to higlighted text nodes that will replace the old nodes.
+        var nodeMap = new Map();
+        var nextTokenGroup = createTokenGroupIterator(tokens, mask);
+        var tokenGroup;
 
-		var startTime = performance.now();
+        while (tokenGroup = nextTokenGroup()) {
 
-		var nodeMap = new Map();
-		var nextTokenGroup = createTokenGroupIterator(tokens, mask);
-		var group;
+            var nextFeatureSet = createSubGroupFeatureSetIterator(
+                tokenGroup.tokens, tokenGroup.pencil);
+            var subGroupFeats;
 
-		while (group = nextTokenGroup()) {
+            while(subGroupFeats = nextFeatureSet()) {
 
-			//clearZeroNodes(group);
+                var newNodes = highlightTokens(
+                    subGroupFeats.tokens, 
+                    subGroupFeats.pencil,
+                    subGroupFeats.leftSpace, 
+                    subGroupFeats.rightSpace
+                );
 
-			var nextFeatureSet = createSubGroupFeatureSetIterator(group.tokens, group.type);
-			var feats;
+                if (nodeMap.has(subGroupFeats.textNode)) {
+                    var replaceNodes = nodeMap.get(subGroupFeats.textNode);
+                    replaceNodes.push.apply(replaceNodes, newNodes); // extend in place
+                } else
+                    nodeMap.set(subGroupFeats.textNode, newNodes);
+            }
+        }
+        nodeMap.forEach((replaceNodes, oldNode) => replaceNodeWithMultiples(oldNode, replaceNodes));
+    }
 
-			while(feats = nextFeatureSet()) {
+    /**
+    * Creates a function that yields token groups according to the nummerical
+    * mask. A token group consists of tokens that should be highlighted together
+    * as in "Franky was [not in school] today." The group is determined by the 
+    * numbers (pencil type) of the mask.
+    *
+    * @param {Array of tokenize.Token} tokens
+    * @param {Array of Number} mask
+    * @return {Object} - features of token group
+    *     @prop {Array of tokenize.Token} tokens - tokens of group
+    *     @prop {Number} pencil - specifies the style of highlighting
+    */
+    function createTokenGroupIterator(tokens, mask) {
 
-				var nodes = highlightTokens(feats.tokens, feats.type,
-					feats.leftSpace, feats.rightSpace);
+        var start = 0;
+        var end = 1;
 
-				if (nodeMap.has(feats.node)) {
-					var newNodes = nodeMap.get(feats.node);
-					newNodes.push.apply(newNodes, nodes); // extend in place
-				}
-				else
-					nodeMap.set(feats.node, nodes);
-			}
-		}
-		nodeMap.forEach((newNodes, oldNode) => replaceNodeWithMultiples(oldNode, newNodes));
+        return function() {
 
-		var endTime = performance.now();
-		console.log('highlight() took ' + (endTime - startTime) + ' ms');
-	}
+            if (start === tokens.length)
+                return null;
+            if (isSpanningType(mask[start])) {
+                while (mask[start] === mask[end])
+                    end++;
+            }
+            var group = {
+                tokens: tokens.slice(start, end),
+                pencil: mask[start]
+            };
+            start = end;
+            end++;
+            return group;
+        };
+    }
 
-	function clearZeroNodes(group) {
+    /**
+    * Returns a token sub group and some features nescessary for highlighting.
+    * A token sub group is a subset of tokens that belongs to a single 
+    * text node.
+    *
+    * @param {Array of tokenize.Token} tokens
+    * @param {Number} pencil - highlighting style type
+    * @return {Object} - features of the sub group
+    *     @prop {Array of tokenize.Token} tokens - of subgroup
+    *     @prop {Number} pencil - higlighting style type
+    *     @prop {DOM Text Node} textNode - text node of sub group
+    *     @prop {Boolean} leftSpace - left space of sub group within highlighting?
+    *     @prop {Boolean} rightSpace - right space of sub group within highlighting?
+    */
+    function createSubGroupFeatureSetIterator(tokens, pencil) {
 
-		if (group.type !== 0)
-			return;
+        var subGroups = getSubGroups(tokens, pencil);
+        var i = 0;
+        var firstPos = 0;
+        var lastPos = subGroups.length-1;
 
-		var zeroNodes = [];
-				
-		for (token of group.tokens)
-			if(zeroNodes.indexOf(token.node) === -1)
-				zeroNodes.push(token.node);
+        return function() {
+            
+            var leftSpace, rightSpace;
+            if(!subGroups[i])
+                return null;
+            // determine left space
+            if (i == firstPos || !isSpanningType(pencil))
+                leftSpace = false;
+            else if (withinSameBlockElement(
+                        subGroups[i][0].textNode, 
+                        subGroups[i-1][0].textNode))
+                leftSpace = true;
+            else
+                leftSpace = false;
+            // determine right space
+            if (i == lastPos  || !isSpanningType(pencil))
+                rightSpace = false;
+            else if (withinSameBlockElement(
+                        subGroups[i][0].textNode,
+                        subGroups[i+1][0].textNode))
+                rightSpace = true;
+            else
+                rightSpace = false;
 
-		zeroNodes.pop();
-		zeroNodes.shift();
+            var tokens = subGroups[i]
+            var textNode = tokens[0].textNode;
+            i++; 
 
-		group.tokens = group.tokens.filter(tok => zeroNodes.indexOf(tok.node) == -1);
-	}
+            return {
+                tokens: tokens,
+                pencil: pencil,
+                textNode: textNode,
+                leftSpace: leftSpace,
+                rightSpace: rightSpace
+            };
+        }
+    }
 
-	function createTokenGroupIterator(tokens, mask) {
+    /**
+    * Returns a token soup group. A token sub group is a subset of tokens 
+    * that belongs to a single text node.
+    * 
+    * @param {Array of tokenize.Token} token
+    * @return {Array of tokenize.Token}  
+    */
+    function getSubGroups(tokens) {
 
-		var start = 0;
-		var end = 1;
+        var start = 0;
+        var end = 1;
+        var subGroups = [];
 
-		return function() {
+        while (start != tokens.length) {
+            try {
+                while (tokens[start].textNode === tokens[end].textNode)
+                    end++;
+            } 
+            catch (e) {
+                if (e.name === 'TypeError') {}
+                else throw e;
+            }
+            subGroups.push(tokens.slice(start, end));      
+            start = end;
+            end++;          
+        };
+        return subGroups;
+    }
 
-			if (start === tokens.length)
-				return null;
+    /**
+    * Returns true if style type (pencil) is a spanning type, 
+    * i.e. neighbouring tokens of this type will highlight together
+    * as in "Franky was [not in school] today." Whereas neighbouring 
+    * tokens of a non spanning type will higlighted separat as in 
+    * "Franky was [not] [in] [school] today.".
+    *
+    * @param {Number} pencil
+    * @return {Boolean}
+    */
+    function isSpanningType(pencil) {
+        return !(pencil % 2);
+    }
 
-			while (mask[start] === mask[end])
-				end++;
+    /**
+    * Return true is the given nodes are within the same block Element.
+    *
+    * @param {DOM Node} node1
+    * @param {DOM Node} node2
+    */
+    function withinSameBlockElement(node1, node2) {
 
-			var group = {
-				tokens: tokens.slice(start, end),
-				type: mask[start]
-			};
+        function getNearestBlockElement(node) {
+            while (node = node.parentElement) {
+                var display = window.getComputedStyle(node).display;
+                if (display === 'block')
+                    return node;
+            }
+        }
+        return getNearestBlockElement(node1) === getNearestBlockElement(node2);
+    }
 
-			start = end;
-			end++;
+    /**
+    * Highlights the given tokens by wrapping them up in a styled
+    * HTML element. Leading and/or trailing space will be returned
+    * as plain text nodes if not within the highlighting.
+    *
+    * @param {Array of tokenize.Token} tokens - to be highlighted
+    * @param {Number} pencil - style type
+    * @param {Boolean} leftSpace - left space within highlighting
+    * @param {Boolean} rightSpace - right space within highlighting
+    * @return {Array of DOM Node}
+    */
+    function highlightTokens(tokens, pencil, leftSpace, rightSpace) {
 
-			return group;
-		};
-	}
+        var nodes = [];
+        var string = tokens.map(tok => tok.form).join('');
+        
+        if (!leftSpace)
+            string = string.replace(/^\s*/, function(match) {
+                nodes.push(document.createTextNode(match));
+                return '';
+            });
 
-	function createSubGroupFeatureSetIterator(tokens, type) {
+        var textNode = document.createTextNode(string);
+        
+        if (pencil in pencilMap) {
+            var element = document.createElement('SPAN');
+            element.classList.add(GLOBAL_CLASS_NAME);
+            element.classList.add(pencilMap[pencil]);
+            element.appendChild(textNode);
+            nodes.push(element);
+        }
+        else {
+            nodes.push(textNode);
+        }
 
-		var subGroups = getSubGroups(tokens, type);
-		var i = 0;
-		var firstPos = 0;
-		var lastPos = subGroups.length-1;
+        if(!rightSpace)
+            textNode.data = textNode.data.replace(/\s*$/, function(match) {
+                nodes.push(document.createTextNode(match));
+                return '';
+            });
 
-		return function() {
-			
-			var leftSpace, rightSpace;
+        return nodes;
+    }
 
-			if(!subGroups[i])
-				return null;
+    /**
+    * Replace a node with multiple nodes
+    *
+    * @param {DOM Node} oldNode
+    * @param {Array of DOM Node} newNodes
+    */
+    function replaceNodeWithMultiples(oldNode, newNodes) {
 
-			// determine left space
-			if (i == firstPos || !isSpanningType(type))
-				leftSpace = false;
-			else if (withinSameBlockElement(
-						subGroups[i][0].node, 
-						subGroups[i-1][0].node))
-				leftSpace = true;
-			else
-				leftSpace = false;
+        var parentNode = oldNode.parentNode;
+        for (newNode of newNodes)
+            parentNode.insertBefore(newNode, oldNode)
+        parentNode.removeChild(oldNode);
+        parentNode.normalize();
+    }
 
-			// determine reight space
-			if (i == lastPos  || !isSpanningType(type))
-				rightSpace = false;
-			else if (withinSameBlockElement(
-						subGroups[i][0].node,
-						subGroups[i+1][0].node))
-				rightSpace = true;
-			else
-				rightSpace = false;
+    /**
+    * Remove all elements added by vink by unwrapping the inner content.
+    */
+    function remove() {
 
-			var tokens = subGroups[i]
-			var node = tokens[0].node;
-			i++; 
+        while(elem = document.querySelector('.' + GLOBAL_CLASS_NAME)) {
+            var parent = elem.parentNode;
+            elem.outerHTML = elem.innerHTML;
+            parent.normalize();
+        }
+    }
 
-			return {
-				tokens: tokens,
-				type: type,
-				node: node,
-				leftSpace: leftSpace,
-				rightSpace: rightSpace
-			};
-		}
-	}
-
-	function getSubGroups(tokens, type) {
-
-		var start = 0;
-		var end = 1;
-		var subGroups = [];
-
-		if (!isSpanningType(type)) {
-			return tokens.map(val => [val]);
-		}
-
-		while (start != tokens.length) {
-
-			try {
-				while (tokens[start].node === tokens[end].node)
-					end++;
-			} 
-			catch (e) {
-				if (e.name === 'TypeError') {}
-				else throw e;
-			}
-
-			subGroups.push(tokens.slice(start, end));
-				
-			start = end;
-			end++;			
-		};
-
-		return subGroups;
-	}
-
-	function isSpanningType(type) {
-		return !(type % 2);
-	}
-
-	function withinSameBlockElement(node1, node2) {
-
-		function getNearestBlockElement(node) {
-			while (node = node.parentElement) {
-				var display = window.getComputedStyle(node).display;
-				if (display === 'block')
-					return node;
-			}
-		}
-		return getNearestBlockElement(node1) === getNearestBlockElement(node2);
-	}
-
-	function highlightTokens(tokens, type, leftSpace, rightSpace) {
-
-		var nodes = [];
-		var string = tokens.map(tok => tok.form).join('');
-		
-		if (!leftSpace)
-			string = string.replace(/^\s*/, function(match) {
-				nodes.push(document.createTextNode(match));
-				return '';
-			});
-
-		var textNode = document.createTextNode(string);
-		
-		if (type in pencilMap) {
-			var element = document.createElement('SPAN');
-			element.classList.add(GLOBAL_CLASS_NAME);
-			element.classList.add(pencilMap[type]);
-			element.appendChild(textNode);
-			nodes.push(element);
-		}
-		else {
-			nodes.push(textNode);
-		}
-
-		if(!rightSpace)
-			textNode.data = textNode.data.replace(/\s*$/, function(match) {
-				nodes.push(document.createTextNode(match));
-				return '';
-			});
-
-		return nodes;
-	}
-
-	function replaceNodeWithMultiples(oldNode, newNodes) {
-
-		var parentNode = oldNode.parentNode;
-		for (newNode of newNodes)
-			parentNode.insertBefore(newNode, oldNode)
-		parentNode.removeChild(oldNode);
-		parentNode.normalize();
-	}
-
-	function remove() {
-
-		while(elem = document.querySelector('.' + GLOBAL_CLASS_NAME)) {
-			var parent = elem.parentNode;
-			elem.outerHTML = elem.innerHTML;
-			parent.normalize();
-		}
-	}
-
-	return {
-		highlight: highlight,
-		remove: remove
-	};
+    /** interfaces of module */
+    return {
+        highlight: highlight,
+        remove: remove
+    };
 
 }());
