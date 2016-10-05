69b0976e123cc960b539c73e89fea07f9ed4a7a8
Mathias <mathiasmaass@gmx.de>
2016-10-05 21:58:36 +0200

    Reimplemented options module.
    - list available markers
    - add new marker by entering a url.

--- options.js
+++ options.js
@@ -1,124 +1,188 @@
 // @module options
 var options = (function(){
 
-    var list;
+    function AccordionList(selector) {
 
-    var labelMap = { 
-        name: 'Name: ', 
-        url: 'URL: ' 
-    };
+        this.drawItem = function(item) {
+            if (this._getIndex(item) > -1)
+                this._refreshItem(item);
+            else
+                this._appendItem(item);
+        }
 
-    var newMarker = {
-        id: 'new',
-        name: 'New Marker',
-        url: ''
-    };
+        this.removeItem = function(item) {
+            var i = this._getIndex(item);
+            this.itemFeats[i][1].remove(); // header
+            this.itemFeats[i][2].remove(); // panel
+            this.itemFeats.splice(i, 1);
+            this.root.accordion('refresh');
+            this.root.accordion('option', 'active', i);
+        }
+
+        this._appendItem = function(item) {
+            var header = item.header();
+            var panel = item.panel();
+            this.itemFeats.push([item, header, panel])
+            this.root.append(header, panel);
+            this.root.accordion('refresh'); 
+            this.root.accordion('option', 'active', this._getIndex(item));
+        }
+
+        this._refreshItem = function(item) {
+            var header = item.header();
+            var panel = item.panel();
+            var i = this._getIndex(item);
+            this.itemFeats[i][1].replaceWith(header);
+            this.itemFeats[i][1] = header;
+            this.itemFeats[i][2].replaceWith(panel);
+            this.itemFeats[i][2] = panel;
+            this.root.accordion('refresh');
+            this.root.accordion('option', 'active', i);
+        }
+
+        this._getIndex = function(item) {
+            for (var i=0; i<this.itemFeats.length; i++)
+                if (this.itemFeats[i][0] === item)
+                    return i;
+            return -1;
+        }
+
+        this.root = $(selector);
+        this.itemFeats = [];
+        this.root.accordion({
+            icons: false,
+            animate: false,
+            heightStyle: 'content'
+        });
+    }
+
+    function MarkerListItem(list, marker) {
+
+        var labelMap = { 
+            name: 'Name: ', 
+            url: 'URL: ',
+            description: 'Description: '
+        };
+
+        this.header = function() {
+            return $('<div>').text(this.marker.name || 'Marker ' + this.marker.id);
+        }
+
+        this.panel = function() {
+            var that = this;
+            var container = $('<div>');
+            for (var key in labelMap) {
+                $('<p>').text(labelMap[key] + that.marker[key]).appendTo(container);
+            }
+            $('<input>', { type: 'button', value: 'Remove' }).on('click', function() {
+                that.removeMarker();
+            }).appendTo(container);
+            return container;
+        }
+
+        this.removeMarker = function() {
+            var that = this;
+            chrome.runtime.getBackgroundPage(function(bg) {
+                bg.markerdb.remove(that.marker.id, function() {
+                    that.list.removeItem(that);
+                });
+            });
+        }
+
+        this.list = list;
+        this.marker = marker;
+        this.list.drawItem(this);
+    }
+
+    function RegisterListItem(list) {
+
+        this.header = function() {
+            return $('<div>').text('Register New Marker');
+        }
+
+        this.registerView = function() {
+            var that = this;
+            var container = $('<div>');
+            $('<label>', { for: 'url' }).text('URL: ').appendTo(container);
+            var urlInput = $('<input>', { type: 'text', name: 'url' }).appendTo(container);
+            $('<input>', { type: 'button', value: 'Register' }).on('click', function() {
+                that.registerMarker(urlInput.val());
+            }).appendTo(container);
+            return container;
+        }
+
+        this.errorView = function(message) {
+            var that = this;
+            var container = $('<div>');
+            $('<h3>').text('Error:').appendTo(container);
+            $('<p>').text(message).appendTo(container);
+            $('<input>', { type: 'button', value: 'Back' }).on('click', function() {
+                that.panel = that.registerView;
+                that.list.drawItem(that);           
+            }).appendTo(container);
+            return container;
+        }
+
+        this.showError = function(message) {
+            this.panel = this.errorView.bind(this, message);
+            this.list.drawItem(this);
+        }
+
+        this.registerMarker = function(url) {
+            var that = this;
+            that.checkInputUrl(url, function() {
+                that.requestSettings(url, function(settings) {
+                    that.saveMarker(url, settings, function(marker) {
+                        new MarkerListItem(that.list, marker);
+                    });
+                });
+            });
+        }
+
+        this.checkInputUrl = function(url, callback) {
+            if (url.search(/^(http:\/\/|https:\/\/)/) === -1)  {
+                this.showError('Need a valid http url that starts with "http://" or "https://".');
+            } else if (callback) {
+                callback();
+            }
+        }
+
+        this.requestSettings = function(url, callback) {
+            var that = this;
+            chrome.runtime.getBackgroundPage(function(bg) {
+                bg.request.requestSettings(url, function(err, settings) {
+                    if (err) {
+                        that.showError(err.message);
+                    } else if (callback) {
+                        callback(settings);
+                    }
+                });
+            });
+        }
+
+        this.saveMarker = function (url, settings, callback) {
+            settings.url = url;
+            chrome.runtime.getBackgroundPage(function(bg) {
+                bg.markerdb.add(settings, function(marker) {
+                    if (callback)
+                        callback(marker);
+                });
+            });          
+        } 
+
+        this.list = list;
+        this.panel = this.registerView;
+        list.drawItem(this);
+    }
 
     function draw() {
-        createList();
-        addItemToList(newMarker);
         chrome.runtime.getBackgroundPage(function(bg) {
+            var list = new AccordionList('#marker-list');
+            new RegisterListItem(list);
             bg.markerdb.get(null, function(markers) {
                 for (var marker of markers) {
-                    addItemToList(marker);
+                    new MarkerListItem(list, marker);
                 }
-            });
-        });
-    }
-
-    function createList() {
-        list = $('#marker-list').accordion({
-            icons: false
-        });
-    }
-
-    // if item of marker id exists -> replace
-    // otherwise -> append
-    function addItemToList(marker) {
-
-        var existing = $('div[marker-id=' + marker.id + ']');
-        if (existing.length) {
-            existing.first().replaceWith(createHeader(marker));
-            existing.last().replaceWith(createPanel(marker));
-        } else {
-            list.append(createHeader(marker));
-            list.append(createPanel(marker));
-        }
-        list.accordion('refresh');
-        list.accordion('option', 'active', 0);
-    }
-
-    function removeItemFromList(marker) {
-        $('div[marker-id=' + marker.id + ']').remove();
-        list.accordion('refresh');
-        list.accordion('option', 'active', 0);
-    }
-
-    function createHeader(marker) {
-        return $('<div>', {'marker-id': marker.id}).text(marker.name);
-    }
-
-    function createPanel(marker) {
-        
-        var panel = $('<div>', {'marker-id': marker.id});
-        
-        for (var key in labelMap) {
-            $('<label>', { for: key }).text(labelMap[key]).appendTo(panel);
-            $('<input>', { type: 'text', name: key, value: marker[key] }).appendTo(panel);
-            $('<br>').appendTo(panel);
-        }
-
-        $('<input>', { type:'button', name:'add', value: 'Add Marker' })
-            .on('click', function() {
-                addMarker(marker);
-            }).appendTo(panel).css('display', marker.id === 'new' ? '' : 'none');
-
-        $('<input>', { type:'button', name:'apply', value: 'Apply Changes' })
-            .on('click', function() {
-                applyChanges(marker);
-            }).appendTo(panel).css('display', marker.id === 'new' ? 'none' : '');
-
-        $('<input>', { type:'button', name:'remove', value: 'Remove Marker' })
-            .on('click', function() {
-                removeMarker(marker);
-            }).appendTo(panel).css('display', marker.id === 'new' ? 'none' : '');
-        
-        return panel;
-    }
-
-    function addMarker(marker) {
-        chrome.runtime.getBackgroundPage(function(bg) {
-            bg.markerdb.add(getInputs(marker), function(newMarker) {
-                addItemToList(marker);
-                addItemToList(newMarker);
-            });
-        });
-    }
-
-    function applyChanges(marker) {
-        chrome.runtime.getBackgroundPage(function(bg) {
-            bg.markerdb.edit(marker.id, getInputs(marker), function(changedMarker) {
-                addItemToList(changedMarker);
-            });
-        });
-    }
-
-    function getInputs(marker) {
-        var infos = {};
-        var inputs = $('div[marker-id=' + marker.id + '] input[type=text]');
-        inputs.each(function(index, element) {
-            var key = $(element).attr('name');
-            var val = element.value;
-            infos[key] = val;
-        });
-        console.log(infos);
-        return infos;
-    }
-    
-    function removeMarker(marker) {
-        chrome.runtime.getBackgroundPage(function(bg) {
-            bg.markerdb.remove(marker.id, function() {
-                removeItemFromList(marker);
             });
         });
     }
