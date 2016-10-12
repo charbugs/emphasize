dd5cf63d1cac2373686cb124c129eb7ba1e29a5d
Mathias <mathiasmaass@gmx.de>
2016-10-12 15:59:09 +0200

    Changed module statuslog to be accessible for proxy.

--- statuslog.js
+++ statuslog.js
@@ -1,84 +1,149 @@
+/** 
+* @module satuslog
+*/
+var statuslog = (function (){
 
-var statuslog = (function(){
+	// <meta name="vink-status-log" markerid="id" inprogress="0" message="foobarbazbam">
 
-	// <meta name="vink-status-log" marker="id" inprogress="0" message="foobarbazbam">
-
+	/** Value for the name attribute of meta elements */
 	const META_NAME = 'vink-status-log';
 
+	/** 
+	* The module's error object
+	*
+	* @param {String} message - error message
+	*/
 	function StatusLogError(message) {
 
 		this.message = message;
 		this.stack = (new Error()).stack;
 	}
-
 	StatusLogError.prototype = Object.create(Error.prototype);
 	StatusLogError.prototype.name = 'StatusLogError';
 
-	function getStatus(attrs) {
+	/**
+	* Respresents the status of a marker in a web page.
+	*
+	* @param {Number} markerId
+	* @param {Number} inprogress - greater then 0 if the marker was called but the highlighting process is not done so far.
+	* @param {String} message - message of marker app.
+	*/
+	function Status(markerId, inprogress, message) {
+		
+		this.markerId = markerId;
+		this.inprogress = inprogress;
+		this.message = message;
+	}
 
-		var meta = document.querySelector('meta[marker="' + attrs.markerId + '"]');
+	/**
+	* Returns the status for a specific marker.
+	* 
+	* @param {Number} markerId
+	* @param {Function} callback - ({jsonisable} err, {Status} status)
+	*/
+	function getStatus(markerId, callback) {
+		
+		var meta = document.querySelector('meta[markerid="'+markerId+'"]');
 		
 		if (meta) {
 
-			return {
-				markerId: meta.getAttribute('marker'),
-				inprogress: meta.getAttribute('inprogress'),
-				message: meta.getAttribute('message')
-			}
-		}
-		else {
+			var status = new Status (
+				markerId,
+				meta.getAttribute('inprogress'),
+				meta.getAttribute('message')
+			);
 
+			if (callback) {
+				callback(null, status);
+			}
+
+		} else {
 			throw new StatusLogError(
 				'Can not return the status for that marker. Status does not exist.');
 		}	
 	}
 
-	function setStatus(attrs) {
+	/**
+	* Set the status for a specific marker.
+	*
+	* @param {Object (Status like)} status
+	* @param {Function} callback - ({jsonisable} err, {jsonsisable} data)
+	*/ 
+	function setStatus(status, callback) {
 
-		if (!document.querySelector('meta[marker="' + attrs.markerId + '"]')) {
+		if (!status.markerId) {
+			throw new StatusLogError(
+				'Can not set the status. No marker id given.');
+		}
+
+		if (!document.querySelector('meta[markerid="' + status.markerId + '"]')) {
 			
 			var meta = document.createElement('meta');
 
 			meta.setAttribute('name', META_NAME);
-			meta.setAttribute('marker', attrs.markerId || '');
-			meta.setAttribute('inprogress', attrs.inprogress || '');
-			meta.setAttribute('message', attrs.message || '');
+			meta.setAttribute('markerid', status.markerId);
+			meta.setAttribute('inprogress', status.inprogress || '');
+			meta.setAttribute('message', status.message || '');
 
 			document.querySelector('head').appendChild(meta);
-		}
-		else {
 
+			if (callback) {
+				callback(null, null);
+			}
+
+		} else {
 			throw new StatusLogError(
-				'Can not set the status for that marker. Status already exists');
+				'Can not set the status for that marker. Status already exists.');
 		}
 	}
 
-	function changeStatus(attrs) {
+	/**
+	* Change the status of a specific marker.
+	* 
+	* @param {Object (Status like)} status
+	* @param {Function} callback - ({jsonisable} err, {jsonsisable} data)
+	*/
+	function changeStatus(status, callback) {
 
-		var meta = document.querySelector('meta[marker="' + attrs.markerId + '"]');
+		var meta = document.querySelector('meta[markerid="' + status.markerId + '"]');
 
 		if (meta) {
 
 			// only the following attributes can be changed
-			if (attrs.inprogress)
-				meta.setAttribute('inprogress', attrs.inprogress);
-			if (attrs.message)
-				meta.setAttribute('message', attrs.message);
-		}
-		else {
+			if (status.hasOwnProperty('inprogress'))
+				meta.setAttribute('inprogress', status.inprogress);
+			if (status.hasOwnProperty('message'))
+				meta.setAttribute('message', status.message);
 
+			if (callback) {
+				callback(null, null);
+			}
+
+		} else {
 			throw new StatusLogError(
 				'Can not change the status for that marker. Status does not exist.');
 		}
 	}
 
-	function removeStatus(attrs) {
+	/**
+	* Remove the status for a specific marker.
+	*
+	* @param {Number} markerId
+	* @param {Function} callback - ({jsonisable} err, {jsonisable} data)
+	*/
+	function removeStatus(markerId, callback) {
 
-		var meta = document.querySelector('meta[marker="' + attrs.markerId + '"]');
+		var meta = document.querySelector('meta[markerid="' + markerId + '"]');
+
 		if (meta) {
+
 			meta.parentElement.removeChild(meta);
-		}
-		else {
+
+			if (callback) {
+				callback(null, null);
+			}
+
+		} else {
 			throw new StatusLogError(
 				'Can not remove status for that marker. Status does not exist.');
 		}
