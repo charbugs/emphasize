6679cba141eabf35fec52ce85c32a228fcf7ac53
Mathias Maaß <mathiasmaass@gmx.de>
2016-10-22 22:45:47 +0200

    Added comments to module options.

--- options.js
+++ options.js
@@ -1,19 +1,35 @@
-
+/** @module options */
 var options = (function() {
 
-    function MarkerListItem(marker, listItems) {
+    /**
+    * Respresents an item of the marker listing 
+    * within the option form.
+    *
+    * @param {markerdb.Marker} marker
+    * @param {Array} markerListItems - a reference to the container 
+    *        that holds all items of the marker list.
+    */
+    function MarkerListItem(marker, markerListItems) {
         
+        /**
+        * Toggles the panel of the marker item.
+        */
         this.togglePanel = function() {
 			this.views.panel = !this.views.panel;
 		};
 
+
+        /**
+        * Removes the marker from the marker database and
+        * and the list.
+        */
         this.removeMarker = function() {
             var that = this;
             chrome.runtime.getBackgroundPage(function(bg) {
                 bg.markerdb.remove(that.marker.id, function() {
-                    for (var i=0; i<that.listItems.length; i++) {
-                        if (that.listItems[i] === that) {
-                            that.listItems.splice(i, 1);    
+                    for (var i=0; i<that.markerListItems.length; i++) {
+                        if (that.markerListItems[i] === that) {
+                            that.markerListItems.splice(i, 1);    
                         }
                     }    
                 });
@@ -21,29 +37,51 @@ var options = (function() {
         };
 
         this.marker = marker;
-        this.listItems = listItems;
+        this.markerListItems = markerListItems;
         this.views = {
             panel: false
         };
     }
 
-    
+    /**
+    * Represents the the list item to register a new marker.
+    *
+    * @param {Array} markerListItems - a reference to the container 
+    *        that holds all items of the marker list.
+    */
     function RegisterListItem(markerListItems) {
         
+        /**
+        * Toggles the panel of the register item.
+        */
         this.togglePanel = function() {
 			this.views.panel = !this.views.panel;
 		};
 
+        /**
+        * Switches between the different panel views of the item.
+        * 
+        * @param {Object} views - witch views should be shown or hide?
+        *       keys are panel names, values are boolean.
+        */
         this.switchView = function(views) {
             this.views.register = views.register || false;
             this.views.success = views.success || false;
             this.views.error = views.error || false;    
         };
 
+        /**
+        * Switches back to the register form.
+        */
          this.backToRegister = function() {
             this.switchView({ register:true });
         };
 
+        /**
+        * Registers a new marker to the system based on 
+        * an url given by user input. The new marker will be
+        * added to the marker list on success.
+        */
         this.registerMarker = function() {
             var that = this;
             if (!that.checkInputUrl(that.inputUrl)) {
@@ -67,6 +105,9 @@ var options = (function() {
             }
         };
 
+        /**
+        * Checks if a url is valid in terms of the system.
+        */
         this.checkInputUrl = function(url) {
             return url.search(/^(http:\/\/|https:\/\/)/) === -1 ? false : true;
         }
@@ -84,7 +125,9 @@ var options = (function() {
         };
     }
 
-    
+    /**
+    * Init and shows the options form.
+    */    
     function draw() {
 
         chrome.runtime.getBackgroundPage(function(bg) { 
@@ -111,6 +154,7 @@ var options = (function() {
 		});
     }    
 
+    /** module interfaces */
     return {
         draw: draw
     };
