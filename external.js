cd8c7a7b36ea91cb3fbf96e293c1dc63051c565a
Mathias Maaß <mathiasmaass@gmx.de>
2016-12-19 18:55:41 +0100

    Module that serves interfaces for interaction with the repository.

---
+++ external.js
@@ -0,0 +1,80 @@
+
+var external = (function() {
+    
+    function handleMessage(message, sender, callback) {
+
+        if (message.command === 'getMarkers') {
+            getMarkers(callback);
+        }
+
+        if (message.command === 'addMarker') {
+            addMarker(message.url, callback);
+        }
+
+        if (message.command === 'removeMarker') {
+            removeMarker(message.url, callback);
+        }
+
+        // Return true here is important, see:
+        // https://developer.chrome.com/extensions/runtime#event-onMessageExternal
+        return true;
+    }
+
+    function handleConnect(port) {
+        
+        if (port.name === 'onMarkerAdded') {
+            registerPortToEvent(port, markerdb.markerAdded);            
+        }
+
+        if (port.name === 'onMarkerRemoved') {
+            registerPortToEvent(port, markerdb.markerRemoved);            
+        }
+    }
+
+    function registerPortToEvent(port, event) {
+        function deliverer(data) {
+            port.postMessage(data);
+        };
+        event.register(deliverer);
+        port.onDisconnect.addListener(function() {
+            event.remove(deliverer);
+        });
+    }
+
+    function getMarkers(callback) {
+        markerdb.get(null, function(markers) {
+            callback(markers);    
+        });
+    }
+
+    function addMarker(url, callback) {
+        markerdb.register(url, function(err, marker) {
+            if (callback)
+                callback(err, marker);
+        });
+    }
+    
+    function removeMarker(url, callback) {
+        markerdb.removeByUrl(url, function(err, marker) {
+            if (callback)
+                callback(err, marker);                
+        });
+    }    
+    
+    function init() {
+        chrome.runtime.onMessageExternal.addListener(handleMessage);
+        chrome.runtime.onConnectExternal.addListener(handleConnect);
+    }
+
+    return {
+
+        init: init
+    };
+
+})();
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    external.init();
+});
