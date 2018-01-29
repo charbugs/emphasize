453e5d77b9ae8a02dcd2ac395abeed57d5e75b85
Mathias Maaß <mathiasmaass@gmx.de>
2018-01-29 15:23:42 +0100

    A module to compile markup from remote before annotating

---
+++ src/webpage/markup-compiler.js
@@ -0,0 +1,110 @@
+
+(function(em) {
+
+	'use strict';
+
+	function* compileToken(item, webPageTokens) {
+		if (item.token < webPageTokens.length) {
+			var _item = Object.assign({}, webPageTokens[item.token], item);
+			delete _item.token;
+			yield _item;
+		}
+	}
+
+	function* compileTokens(item, webPageTokens) {
+		for (var token of item.tokens) {
+			var _item = Object.assign({}, item);
+			delete _item.tokens;
+			_item.token = token;
+			yield* compileToken(_item, webPageTokens);
+		}
+	}
+
+	function* compileGroupSameNode(item, webPageTokens, first, last) {
+		var begin = first.begin;
+		var end = last.end;
+		var node = first.node;
+		var form = node.data.slice(begin, end);
+		var _item = Object.assign({ begin, end, form, node }, item);
+		delete _item.group;
+		yield _item;
+	}
+
+	function* compileGroupCrossNodes(item, webPageTokens, first, last) {
+		var embeddedNodes = new Set();
+		webPageTokens.slice(item.group.first, item.group.last + 1)
+			.forEach(token => embeddedNodes.add(token.node));
+		embeddedNodes = Array.from(embeddedNodes);
+		embeddedNodes.shift(); // === first.node
+		embeddedNodes.pop(); // === last.node
+
+		yield {
+			begin: first.begin,
+			end: first.node.data.length,
+			form: first.node.data.slice(first.begin),
+			node: first.node
+		};
+
+		for (var node of embeddedNodes) {
+			yield {
+				begin: 0,
+				end: node.data.length,
+				form: node.data,
+				node: node
+			};
+		}
+
+		yield {
+			begin: 0,
+			end: last.end,
+			form: last.node.data.slice(0, last.end),
+			node: last.node
+		};
+	}
+
+	function* compileGroup(item, webPageTokens) {
+		var first, last;
+		
+		if (!(first = webPageTokens[item.group.first]))
+			return;
+		if (!(last = webPageTokens[item.group.last]))
+			last = webPageTokens[webPageTokens.length - 1];	
+
+		if (first.node === last.node)
+			yield* compileGroupSameNode(item, webPageTokens, first, last);
+		else 
+			yield* compileGroupCrossNodes(item, webPageTokens, first, last);
+	}
+
+	function* compileGroups(item, webPageTokens) {
+		for (var group of item.groups) {
+			var _item = Object.assign({}, item);
+			delete _item.groups;
+			_item.group = group;
+			yield* compileGroup(_item, webPageTokens);
+		}
+	}
+
+	function* compile(markup, webPageTokens) {
+
+		for (var item of markup) {
+			if ('token' in item)
+				yield* compileToken(item, webPageTokens);
+			else if ('tokens' in item)
+				yield* compileTokens(item, webPageTokens);
+			else if ('group' in item)
+				yield* compileGroup(item, webPageTokens);
+			else if ('groups' in item)
+				yield* compileGroups(item, webPageTokens);
+		}
+	}
+
+	function compileMarkup(markup, webPageTokens) {
+		return Array.from(compile(markup, webPageTokens));
+	}
+
+	em.markupCompiler = {
+		compileMarkup
+	};
+
+})(emphasize);
