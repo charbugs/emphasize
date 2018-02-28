f85f7ad54cba4192d55c504e2e932f0f3f620794
Mathias Maaß <mathiasmaass@gmx.de>
2018-02-28 16:40:15 +0100

    Reactjs environment for popup package

---
+++ src/popup/jsx/src/popup.jsx
@@ -0,0 +1,2 @@
+
+ReactDOM.render(<h3>Hello Popup</h3>, document.querySelector('#root'));
--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,52 +1,6 @@
 
-var _popup = (function() {
-
-	'use strict';
-
-	/**
-	 * Informs that the menu is disabled on the current tab.
-	 */
-	function showTabDisabledMessage() {
-		document.body.innerHTML =
-			'<div class="w3-panel">Cannot work on this browser tab.</div>';
-	}
-
-	/**
-	 * Binds model objects to the html content using vue js.
-	 */
-	async function init() {
-
-		var bg = chrome.extension.getBackgroundPage();
-		var injection = bg.emphasize.app.injection;
-		var menuContainer = bg.emphasize.app.menuContainer;
-
-		try {
-			var tabId = await injection.connectWebPage();
-			var menu = await menuContainer.get(tabId);
-			
-			new Vue({
-				el: '#menu',
-				data: {
-					menu: menu,
-					version: chrome.runtime.getManifest().version
-				}
-			});
-		} 
-		catch(err) {
-			if (err.name === 'InjectionError')
-				showTabDisabledMessage();
-			else
-				throw err;
-		}
-	}
-
-	return {
-		init
-	};
-
-})();
-
-
-document.addEventListener('DOMContentLoaded', function() {
-	_popup.init();
-});
+ReactDOM.render(React.createElement(
+  'h3',
+  null,
+  'Hello Popup'
+), document.querySelector('#root'));
